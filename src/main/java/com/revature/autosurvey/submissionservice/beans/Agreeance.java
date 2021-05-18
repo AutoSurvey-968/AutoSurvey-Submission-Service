a77bb17318779ed68fe71120978f5f287d39297d
Mat <Mat.R.Kerwin@gmail.com>
2021-05-18 13:38:28 -0700

    response bean

---
+++ src/main/java/com/revature/autosurvey/submissionservice/beans/Agreeance.java
@@ -0,0 +1,5 @@
+package com.revature.autosurvey.submissionservice.beans;
+
+public enum Agreeance {
+
+}
---
+++ src/main/java/com/revature/autosurvey/submissionservice/beans/Location.java
@@ -0,0 +1,5 @@
+package com.revature.autosurvey.submissionservice.beans;
+
+public enum Location {
+	NY, RESTON, USF, UTA, OTHER; //Get the rest of the possible locations from the survey
+}
