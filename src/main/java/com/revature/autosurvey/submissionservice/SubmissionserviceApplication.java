14a6697fcdaba500cd582355e17d5209e6cd8f1d
DuskDaleSpider <dakota.ray.clark@gmail.com>
2021-05-19 12:42:45 -0400

    Set up Eureka Client
    + eureka client dependency
    + Enabled Eureka
    + Added the necessary config for client

--- src/main/java/com/revature/autosurvey/submissionservice/SubmissionserviceApplication.java
+++ src/main/java/com/revature/autosurvey/submissionservice/SubmissionserviceApplication.java
@@ -2,8 +2,10 @@ package com.revature.autosurvey.submissionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SubmissionserviceApplication {
 
 	public static void main(String[] args) {
