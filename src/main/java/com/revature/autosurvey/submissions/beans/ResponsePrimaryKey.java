77ae55d83d3e0689950cd4f28980521e161a5943
Mat <Mat.R.Kerwin@gmail.com>
2021-05-19 11:00:12 -0700

    I, Matt, wrote all this code and did not eat a lunchable while Chris controlled my screen and wrote it

--- src/main/java/com/revature/autosurvey/submissions/beans/ResponsePrimaryKey.java
+++ src/main/java/com/revature/autosurvey/submissions/beans/ResponsePrimaryKey.java
@@ -1,13 +1,25 @@
 package com.revature.autosurvey.submissions.beans;
 
+import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 
+@PrimaryKeyClass
 public class ResponsePrimaryKey {
-	@PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+	@PrimaryKeyColumn(
+			type = PrimaryKeyType.CLUSTERED,
+			ordinal = 2,
+			ordering = Ordering.DESCENDING)
 	private int id;
-	@PrimaryKeyColumn(ordinal = 0)
+	@PrimaryKeyColumn(
+			type = PrimaryKeyType.CLUSTERED,
+			ordinal = 1,
+			ordering = Ordering.DESCENDING)
 	private String week;
-	@PrimaryKeyColumn(ordinal = 1)
+	@PrimaryKeyColumn(
+			type = PrimaryKeyType.PARTITIONED,
+			ordinal = 0,
+			ordering = Ordering.DESCENDING)
 	private int batch;
 }
