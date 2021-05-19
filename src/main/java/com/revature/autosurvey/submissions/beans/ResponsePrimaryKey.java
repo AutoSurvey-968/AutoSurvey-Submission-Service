2b9d768a2ddaec9e927edfcd5ed07bd98b1c4d8a
Christopher Morrison <43414d@gmail.com>
2021-05-19 13:48:16 -0500

    fixed key organization

--- src/main/java/com/revature/autosurvey/submissions/beans/ResponsePrimaryKey.java
+++ src/main/java/com/revature/autosurvey/submissions/beans/ResponsePrimaryKey.java
@@ -1,5 +1,7 @@
 package com.revature.autosurvey.submissions.beans;
 
+import java.util.UUID;
+
 import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
@@ -8,17 +10,17 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 @PrimaryKeyClass
 public class ResponsePrimaryKey {
 	@PrimaryKeyColumn(
-			type = PrimaryKeyType.CLUSTERED,
+			type = PrimaryKeyType.PARTITIONED,
 			ordinal = 2,
 			ordering = Ordering.DESCENDING)
-	private int id;
+	private UUID id;
 	@PrimaryKeyColumn(
 			type = PrimaryKeyType.CLUSTERED,
 			ordinal = 1,
 			ordering = Ordering.DESCENDING)
 	private String week;
 	@PrimaryKeyColumn(
-			type = PrimaryKeyType.PARTITIONED,
+			type = PrimaryKeyType.CLUSTERED,
 			ordinal = 0,
 			ordering = Ordering.DESCENDING)
 	private int batch;
--- src/main/java/com/revature/autosurvey/submissions/data/ResponseRepository.java
+++ src/main/java/com/revature/autosurvey/submissions/data/ResponseRepository.java
@@ -1,12 +1,18 @@
 package com.revature.autosurvey.submissions.data;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.autosurvey.submissions.beans.Response;
 import com.revature.autosurvey.submissions.beans.ResponsePrimaryKey;
 
+import reactor.core.publisher.Flux;
+
 @Repository
 public interface ResponseRepository extends ReactiveCassandraRepository<Response, ResponsePrimaryKey>{
-//We might have to change this primary key
+	@AllowFiltering
+	public Flux<Response> findAllByKeyBatch(int batch);
+	@AllowFiltering
+	public Flux<Response> findAllByKeyWeek(String week);
 }
