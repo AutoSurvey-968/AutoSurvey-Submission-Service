f7a8fbfa4866ac59baa6fe559724ce6d37e2847a
Mat <Mat.R.Kerwin@gmail.com>
2021-05-25 08:36:31 -0700

    utilities packages

--- src/test/java/com/revature/autosurvey/submissions/service/ResponseServiceTest.java
+++ src/test/java/com/revature/autosurvey/submissions/service/ResponseServiceTest.java
@@ -93,25 +93,6 @@ public class ResponseServiceTest {
 	}
 	
 	@Test
-	void bigSplitReturnsCorrectList() {
-		String string = "this is a value,\"This is a, value wi,th a ,b,unch ,of com,mas ,i,n it\",and this is another value,and so is this";
-		List<String> result = new ArrayList<>();
-		result.add("this is a value");
-		result.add("\"This is a, value wi,th a ,b,unch ,of com,mas ,i,n it\"");
-		result.add("and this is another value");
-		result.add("and so is this");
-		assertEquals(result, responseService.bigSplit(string));
-	}
-	
-	@Test
-	void timeLongFromStringReturnsTimeLong() {
-		String string = "3/3/2020  14:08:17";
-		Long timeLong = 1583244497000L;
-		
-		assertEquals(timeLong, responseService.timeLongFromString(string));
-	}
-	
-	@Test
 	void buildResponseFromCsvLineReturnsResponse() {
 		Response res = new Response();
 		UUID surveyId = UUID.fromString("11111111-1111-1111-1111-111111111001");
@@ -136,84 +117,6 @@ public class ResponseServiceTest {
 	@Test
 	void addResponsesFromFileReturns() {
 		//I do not know how to generate a Flux<FilePart> so I gotta figure that out to write this test
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnumA() {
-		String weekString = "Week A";
-		assertEquals(TrainingWeek.A, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnumB() {
-		String weekString = "Week B";
-		assertEquals(TrainingWeek.B, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum1() {
-		String weekString = "Week 1";
-		assertEquals(TrainingWeek.ONE, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum2() {
-		String weekString = "Week 2";
-		assertEquals(TrainingWeek.TWO, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum3() {
-		String weekString = "Week 3";
-		assertEquals(TrainingWeek.THREE, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum4() {
-		String weekString = "Week 4";
-		assertEquals(TrainingWeek.FOUR, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum5() {
-		String weekString = "Week 5";
-		assertEquals(TrainingWeek.FIVE, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum6() {
-		String weekString = "Week 6";
-		assertEquals(TrainingWeek.SIX, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum7() {
-		String weekString = "Week 7";
-		assertEquals(TrainingWeek.SEVEN, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum8() {
-		String weekString = "Week 8";
-		assertEquals(TrainingWeek.EIGHT, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum9() {
-		String weekString = "Week 9";
-		assertEquals(TrainingWeek.NINE, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsTrainingWeekEnum10() {
-		String weekString = "Week 10";
-		assertEquals(TrainingWeek.TEN, responseService.getTrainingWeekFromString(weekString));
-	}
-	
-	@Test
-	void getTrainingWeekFromStringReturnsNull() {
-		String weekString = "Any Other Value";
-		assertEquals(null, responseService.getTrainingWeekFromString(weekString));
 	}
 
 	@Test
