ef92803440d5fbe4a2082e6d9a0e93bb02367b18
NGelezinis <ngelez2@uic.edu>
2021-05-25 16:05:21 -0500

    update

--- src/test/java/com/revature/autosurvey/submissions/utils/UtilitiesTest.java
+++ src/test/java/com/revature/autosurvey/submissions/utils/UtilitiesTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.revature.autosurvey.submissions.beans.TrainingWeek;
